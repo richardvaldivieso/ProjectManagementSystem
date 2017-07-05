ecdba809855c281b2e3b507b207dca0a477e6e86
richardvaldivieso <richardvaldiviesom@gmail.com>
2017-07-04 22:36:59 -0400

    Follow the same pattern of the gradle file on M8 project

--- core/availability-service/src/main/java/com/pms/availabilityservice/AvailabilityServiceApplication.java
+++ core/availability-service/src/main/java/com/pms/availabilityservice/AvailabilityServiceApplication.java
@@ -1,5 +1,7 @@
 package com.pms.availabilityservice;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class AvailabilityServiceApplication {
     public static void main(String[] args) {
